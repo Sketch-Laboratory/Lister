f683dbdfae48cfc97a1b058d5fd68cb4f1acb5f3
Sasarino MARi <develop.mari@gmail.com>
2020-08-02 11:04:52 +0900

    테스트 메뉴와 Recycler Injector 추가

--- app/src/main/java/com/sasarinomari/twitterlistviewer/MainActivity.kt
+++ app/src/main/java/com/sasarinomari/twitterlistviewer/MainActivity.kt
@@ -1,8 +1,15 @@
 package com.sasarinomari.twitterlistviewer
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import androidx.appcompat.app.AppCompatActivity
+import com.sasarinomari.dialog.MaterialDialog
+import com.sasarinomari.dialog.MaterialDialogAdapter
+import com.sasarinomari.dialog.SweetDialogAdapter
 import com.sasarinomari.twitter.TwitterAdapter
 import com.sasarinomari.twitter.authenticate.TokenManagementActivity
 
@@ -10,8 +17,36 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        TwitterAdapter.TwitterInterface.setOAuthConsumer(
-            getString(R.string.consumerKey), getString(R.string.consumerSecret))
-        startActivity(Intent(this, TokenManagementActivity::class.java))
+        val list = ListView(this)
+        setContentView(list)
+
+        val values = arrayOf(
+            "Open Twitter.TokenManagementActivity",
+            "Material Dialog Test",
+            "Sweet Alert Dialog Test"
+        )
+
+        list.adapter = ArrayAdapter(this,
+            android.R.layout.simple_list_item_1, android.R.id.text1, values
+        )
+        list.setOnItemClickListener { _, _, position, _ ->
+            when(position) {
+                0 -> {
+                    TwitterAdapter.TwitterInterface.setOAuthConsumer(
+                        getString(R.string.consumerKey), getString(R.string.consumerSecret))
+                    startActivity(Intent(this, TokenManagementActivity::class.java))
+                }
+                1 -> {
+                    MaterialDialogAdapter(this)
+                        .message("Hello, World!")
+                        .show()
+                }
+                2 -> {
+                    SweetDialogAdapter(this)
+                        .message("Hello, World!")
+                        .show()
+                }
+            }
+        }
     }
 }
